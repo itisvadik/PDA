065765fb9e225e17df60c583cdf466effe506f36
vozix <98023026+itisvadik@users.noreply.github.com>
2026-06-15 00:45:18 +0300

    Add files via upload

---
+++ build.py
@@ -0,0 +1,31 @@
+import re
+import subprocess
+import sys
+
+with open("pda_gift_app.py", "r", encoding="utf-8") as f:
+    content = f.read()
+
+match = re.search(r'PDA_VERSION\s*=\s*"([^"]+)"', content)
+
+version = match.group(1)
+
+subprocess.run([
+    sys.executable,
+    "-m",
+    "PyInstaller",
+    "--onefile",
+    "--noconsole",
+    "--name",
+    f"КПК {version}",
+    "--add-data",
+    "button.wav;.",
+    "--add-data",
+    "photo_lghkbq.jpg;.",
+    "--add-data",
+    "arhive_video_NO3826.mp4;.",
+    "--add-data",
+    "arhive_video_NO67.mp4;.",
+    "--add-data",
+    "secret.txt;.",
+    "pda_gift_app.py"
+])
